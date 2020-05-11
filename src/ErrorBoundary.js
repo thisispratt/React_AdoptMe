d95ef1d1ef82c3a79002977f36a247bab3d5944a
prabhat_tiwari <prabhat.avb@gmail.com>
2020-05-11 23:44:23 +0530

    add ErrorBoundary to Details Route

---
+++ src/ErrorBoundary.js
@@ -0,0 +1,42 @@
+//mostly code from reactjs.org/docs/error-boundaries.html
+
+import React from "react";
+import { Link, Redirect } from "@reach/router";
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false, redirect: false };
+  }
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    console.error("ErrorBoundary caught an error", error, info);
+  }
+
+  //called immediately after state change or new props . Not called on initial render.
+  componentDidUpdate() {
+    if (this.state.hasError) {
+      setTimeout(() => this.setState({ redirect: true }), 5000);
+    }
+  }
+  render() {
+    if (this.state.redirect) {
+      return <Redirect to="/" />;
+    }
+    if (this.state.hasError) {
+      return (
+        <h1>
+          There was an error with this listing. <Link to="/">Click here</Link>{" "}
+          to go back to the home page or wait five seconds.
+        </h1>
+      );
+    }
+    //render needs to return something otherwise it would block all of its child components.
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
--- src/details.js
+++ src/details.js
@@ -1,6 +1,14 @@
 import React from "react";
 import pet from "@frontendmasters/pet";
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
+
+//used to replace special html entities from the api response.(inside the description for animals)
+// const entities = {
+//   "&#039;": "'",
+//   "&quot;": '"',
+//   // add more if needed
+// };
 
 class Details extends React.Component {
   constructor(props) {
@@ -41,10 +49,18 @@ class Details extends React.Component {
           <h1>{name}</h1>
           <h2>{animal + " - " + breed + " - " + location}</h2>
           <button>Adopt {name}</button>
-          <p>{description}</p>
+          <p>{description}</p>{" "}
+          {/* {description.replace(/&#?\w+;/, (match) => entities[match])} */}
         </div>
       </div>
     );
   }
 }
-export default Details;
+
+export default function DetailsWithErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  );
+}
